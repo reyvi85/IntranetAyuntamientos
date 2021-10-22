a1ea9cd27f8a3c966b7333c19be089ad86da636d
reyvi <reyvi85@gmail.com>
2021-10-22 12:00:06 -0400

    Vista publica de Comercios full

--- resources/views/livewire/front/partial/businessBlock.blade.php
+++ resources/views/livewire/front/partial/businessBlock.blade.php
@@ -9,9 +9,10 @@
                     <img class="card-img-top" src="{{asset((is_null($item->logo) || empty($item->logo))?'images/no-image.jpg':$item->logo)}}" alt="{{$item->name}}"  title="{{$item->name}}" width="100%" height="248px">
                     <div class="card-body">
                         <h5 class="card-title">{{$item->name}}</h5>
-                        <p class="card-text"><span class="font-weight-bold">Descripción:</span> {{$item->description}}</p>
-                        <p class="card-text"><i class="fas fa-address-card"></i> <span class="font-weight-bold">Dirección:</span> {{$item->direccion}}</p>
-                        <p class="card-text"><i class="fas fa-phone"></i> <a href="tel:{{$item->telefono}}">{{$item->telefono}}</a> <br>
+                        <p class="card-text"><span class="font-weight-bold">Descripción:</span> {{$item->description}}<br><br>
+                        <i class="fas fa-address-card"></i> <span class="font-weight-bold">Dirección:</span> {{$item->direccion}}<br>
+                            <i class="fas fa-bookmark" title="Categoría"></i> {{$item->category_busine->name}} <br>
+                        <i class="fas fa-phone"></i> <a href="tel:{{$item->telefono}}">{{$item->telefono}}</a> <br>
                             <i class="fas fa-envelope"></i> <a href="mailto:{{$item->email}}">{{$item->email}}</a> <br>
                             <i class="fas fa-globe"></i> <a href="{{$item->url_web}}" target="_blank">{{$item->url_web}}</a>
                         </p>
--- resources/views/livewire/front/partial/businessList.blade.php
+++ resources/views/livewire/front/partial/businessList.blade.php
@@ -10,6 +10,7 @@
                     <h4>{{$item->name}}</h4>
                     <p>
                         <span class="font-weight-bold">Descripción:</span> {{$item->description}}<br>
+                        <i class="fas fa-bookmark"></i> {{$item->category_busine->name}} <br>
                         <i class="fas fa-phone"></i> <a href="tel:{{$item->telefono}}">{{$item->telefono}}</a> &nbsp;
                         <i class="fas fa-envelope"></i> <a href="mailto:{{$item->email}}">{{$item->email}}</a> <br>
                         <i class="fas fa-globe"></i> <a href="{{$item->url_web}}" target="_blank">{{$item->url_web}}</a>
