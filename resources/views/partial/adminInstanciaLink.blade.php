1335ac98ffb37a53dab1c27eb45faa214030d7bb
reyvi85 <reyvi85@gmail.com>
2021-10-27 20:44:39 -0400

    MOdulo telefonos de interes full

--- resources/views/partial/adminInstanciaLink.blade.php
+++ resources/views/partial/adminInstanciaLink.blade.php
@@ -1,6 +1,4 @@
 <li class="nav-item">
     <a class="nav-link" href="{{route('usuario.gestor')}}"><i class="fas fa-users"></i> Usuarios</a>
 </li>
-<li class="nav-item">
-    <a class="nav-link" href="{{route('gestion.phones')}}"><i class="fas fa-phone"></i> Teléfonos</a>
-</li>
+@include('partial.gestorInstanciaLink')
--- resources/views/partial/gestorInstanciaLink.blade.php
+++ resources/views/partial/gestorInstanciaLink.blade.php
@@ -1,3 +1,11 @@
-<li class="nav-item">
-    <a class="nav-link" href="{{route('gestion.business')}}"><i class="fas fa-euro-sign"></i> Comercios</a>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-mouse-pointer"></i> Módulos
+    </a>
+    <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdownMenuLink">
+
+        <a class="dropdown-item" href="{{route('gestion.business')}}"><i class="fas fa-euro-sign"></i> Comercios</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="{{route('gestion.phones')}}"><i class="fas fa-phone"></i> Teléfonos de interés</a>
+    </div>
 </li>
