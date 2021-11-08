c0543b2ed75a10cfdcab5147e93382b6a2768b31
reyvi85 <reyvi85@gmail.com>
2021-11-08 16:49:07 -0500

    Menu de acceso a modulos. Componente

--- resources/views/livewire/menu-access-modules-component.blade.php
+++ resources/views/livewire/menu-access-modules-component.blade.php
@@ -1,12 +1,15 @@
 <div>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-mouse-pointer"></i> Módulos
-        </a>
-        <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdownMenuLink">
-            @foreach($modulos as $item)
-                <a class="dropdown-item" href="{{route($item['routeName'])}}"><i class="fas {{$item['icon']}}"></i> {{$item['modulo']}}</a>
-            @endforeach
-        </div>
-    </li>
+    @if ($modulos->count())
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-mouse-pointer"></i> Módulos
+            </a>
+            <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdownMenuLink">
+                @foreach($modulos as $item)
+                    <a class="dropdown-item" href="{{route($item['routeName'])}}"><i class="fas {{$item['icon']}}"></i> {{$item['modulo']}}</a>
+                @endforeach
+            </div>
+        </li>
+    @endif
+
 </div>
