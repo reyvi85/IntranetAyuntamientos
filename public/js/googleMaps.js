98a6b4b28237ac858efbcef6247f40de55baec2a
reyvi <reyvi85@gmail.com>
2021-11-15 15:53:47 -0500

    Mapas en las instancias

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -20,3 +20,16 @@ function resetMap(lat, lng, capa='map'){
     document.getElementById(capa).innerHTML='';
     initMap(lat, lng, capa);
 }
+
+function showMap(lat, lng, capa='map') {
+    let map;
+    map = new google.maps.Map(document.getElementById(capa), {
+        center: {lat: lat, lng: lng },
+        zoom: 10,
+    });
+    var marker = new google.maps.Marker({
+        position: {lat: lat, lng: lng},
+        map: map,
+        draggable:false
+    });
+}
