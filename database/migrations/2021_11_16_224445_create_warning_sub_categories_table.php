1a93af8e2abf12cea4b578e58f6f570f8a7da3f8
reyvi85 <reyvi85@gmail.com>
2021-11-16 19:41:39 -0500

    Creando Modelos del Modulo de Avisos

--- database/migrations/2021_11_16_224445_create_warning_sub_categories_table.php
+++ database/migrations/2021_11_16_224445_create_warning_sub_categories_table.php
@@ -16,7 +16,7 @@ class CreateWarningSubCategoriesTable extends Migration
         Schema::create('warning_sub_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedSmallInteger('warning_category_id');
+            $table->unsignedBigInteger('warning_category_id');
             $table->timestamps();
 
             $table->foreign('warning_category_id')->references('id')->on('warning_categories')->onDelete('cascade');
