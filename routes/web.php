944cf1922180e7606d82b066b2e8b59ca3d6b781
reyvi <reyvi85@gmail.com>
2021-12-03 11:34:27 -0500

    Gestion de Noticias. CRUD Finalizado. Agregando rutas de modulos

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::prefix('gestion')->middleware('auth')->group(function (){
     Route::middleware(['CheckPermissionModules','checkRol:Super-Administrador,Administrador-Instancia,Gestor-Instancia'])->group(function(){
         Route::get('/comercios', BusinessComponent::class)->name('gestion.business');
         Route::get('/telefonos', InterestPhonesComponent::class)->name('gestion.phones');
-
+        Route::get('/noticias', NoticiasComponent::class)->name('gestion.noticias');
         Route::get('/notificaciones', function(){
             return view('livewire.administrator.notification-component');
         })->name('gestion.notifications');
