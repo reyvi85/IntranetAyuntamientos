c66d04f28b69f27df3297680ff171b0429446832
reyvi85 <reyvi85@gmail.com>
2022-02-06 19:54:35 -0600

    Modificando acceso a recursos del  API-2

--- app/Scopes/UserInstanceScope.php
+++ app/Scopes/UserInstanceScope.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
 
 class UserInstanceScope implements Scope
 {
@@ -16,10 +15,6 @@ class UserInstanceScope implements Scope
     {
         if(Auth::check() && Auth::user()->rol != 'Super-Administrador'){
             $builder->where('instance_id',Auth::user()->instance_id);
-        }else{
-            $builder->whereHas('instance', function (Builder $q){
-                $q->where('key',request()->token_inst);
-            });
         }
     }
 }
