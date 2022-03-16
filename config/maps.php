75d9fa62670edcc89a7b8a0c8cd695f70a56009c
reyvi85 <reyvi85@gmail.com>
2022-03-16 18:49:42 -0500

    Agregando API KEY de Google Maps

--- config/maps.php
+++ config/maps.php
@@ -2,7 +2,7 @@
 
 return[
 
-    'API_KEY_GOOGLE_MAP'=>env('API_KEY_GOOGLE_MAP', 'AIzaSyBCKiIqCdZGrVxx06LSbe7uG3zXOq1Cz5k'),
+    'API_KEY_GOOGLE_MAP'=>env('API_KEY_GOOGLE_MAP', 'AIzaSyAtcXvHAT0NpJtfu7fIfRBRymWUAllIBlw'),
 
     'lat_default'=>40.4165,
     'lng_default'=>-3.70256,
