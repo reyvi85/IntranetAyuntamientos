db13438003c0c609ea4726620c43a9cde39c9317
Reyvi Bilbao Betancourt <reyvi85@gmail.com>
2022-11-02 17:31:44 -0400

    Haciendo arreglos en los comercios

--- resources/views/livewire/front/partial/businessBlock.blade.php
+++ resources/views/livewire/front/partial/businessBlock.blade.php
@@ -8,7 +8,11 @@
                 <div class="card">
                     <img class="card-img-top" src="{{asset((is_null($item->logo) || empty($item->logo))?'images/no-image.jpg':$item->logo)}}" alt="{{$item->name}}"  title="{{$item->name}}" width="100%" height="248px">
                     <div class="card-body">
-                        <h5 class="card-title">{{$item->name}}</h5>
+                        @if (!empty($item->url_web))
+                        <h5 class="card-title"><a href="{{$item->url_web}}" target="_blank">{{$item->name}}</a></h5>
+                        @else
+                            <h5 class="card-title">{{$item->name}}</h5>
+                        @endif
                         <p class="card-text"><span class="font-weight-bold">Descripción:</span> {{$item->description}}<br><br>
                         <i class="fas fa-address-card"></i> <span class="font-weight-bold">Dirección:</span> {{$item->direccion}}<br>
                             <i class="fas fa-bookmark" title="Categoría"></i> {{$item->category_busine->name}} <br>
@@ -18,7 +22,6 @@
                             @if (!empty($item->email))
                                 <i class="fas fa-envelope"></i> <a href="mailto:{{$item->email}}">{{$item->email}}</a> <br>
                             @endif
-                            <i class="fas fa-globe"></i> <a href="{{$item->url_web}}" target="_blank">{{$item->url_web}}</a>
                         </p>
 
                     </div>
