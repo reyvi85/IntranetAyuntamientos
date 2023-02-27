f9366eb0f2eb149cb2b66169c28a52f706e23eed
Reyvi Bilbao Betancourt <reyvi85@gmail.com>
2023-02-27 17:15:57 -0500

    Creando email de confirmacion de ruta reservada

--- resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
+++ resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
@@ -2,5 +2,3 @@
 <p>Su reserva ha sido <b>Confirmada</b> para la fecha <b>{{$data->fecha_reserva}}</b>.</p>
 <p>La ruta seleccionada es: <b>{{$data->route->name}}, </b> la cual contiene <b>{{$data->route->route_intermediates->count()}} puntos intermedios</b>
     y su precio es de <b>{{$data->route->price}} euros!</b></p>
-<hr>
-@include('livewire.administrator.routes.viewRoute', ['route'=>$data->route])
