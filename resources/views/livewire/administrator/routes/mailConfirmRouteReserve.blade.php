795a1ce32d05282bb2eae69f7951dfcd2adbc000
Reyvi Bilbao Betancourt <reyvi85@gmail.com>
2023-02-27 16:49:42 -0500

    Creando email de confirmacion de ruta reservada

--- resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
+++ resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
@@ -1,5 +1,5 @@
 <p>Estimad@, <b>{{$data->user->name}}</b></p>
-<p>Su reserva ha sido <b>Confirmada</b> para la fecha <b>{{$data->fecha_reserva}}</b>
-    La ruta seleccionada es: <b>{{$data->route->name}}, </b> la cual contiene {{$data->route->route_intermediates->count()}} puntos intermedios
-    y su precio es de {{$data->route->price}} euros!</p>
+<p>Su reserva ha sido <b>Confirmada</b> para la fecha <b>{{$data->fecha_reserva}}</b>.</p>
+<p>La ruta seleccionada es: <b>{{$data->route->name}}, </b> la cual contiene <b>{{$data->route->route_intermediates->count()}} puntos intermedios</b>
+    y su precio es de <b>{{$data->route->price}} euros!</b></p>
 
