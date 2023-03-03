4549ba5f8efa80280c27aba4daf4a511dc48ce2b
Reyvi Bilbao Betancourt <reyvi85@gmail.com>
2023-03-03 11:13:49 -0500

    Corrigiendo informacion en el correo a enviar

--- resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
+++ resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
@@ -1,4 +1,4 @@
 <p>Estimad@, <b>{{$data->user->name}}</b></p>
 <p>Su reserva ha sido <b>Confirmada</b> para la fecha <b>{{$data->fecha_reserva}}</b> para <b>{{$data->num_person}} personas</b>.</p>
-<p>La ruta seleccionada es: <b>{{$data->route->name}}, </b> la cual comienza <b>{{$data->route->inicio_ruta_name}}</b>
+<p>La ruta seleccionada es: <b>{{$data->route->name}}, </b> la cual comienza en <b>{{$data->route->inicio_ruta_name}}</b>
     y su precio es de <b>{{$data->route->price * $data->num_person}} euros!</b></p>
