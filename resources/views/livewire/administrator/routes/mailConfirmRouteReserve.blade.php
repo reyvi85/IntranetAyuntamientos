f0470d221f564aa072f208a64f6d5dcbeb28ec01
Reyvi Bilbao Betancourt <reyvi85@gmail.com>
2023-04-10 21:45:13 -0400

    MOdificando fecha de correo de confirmacion de ruta

--- resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
+++ resources/views/livewire/administrator/routes/mailConfirmRouteReserve.blade.php
@@ -1,5 +1,4 @@
-@inject('carbon','\Carbon\Carbon')
 <p>Estimad@, <b>{{$data->user->name}}</b></p>
-<p>Su reserva ha sido <b>Confirmada</b> para la fecha <b>{{carbon::parse($data->fecha_reserva)->isoFormat('dddd D \d\e MMMM \d\e\l Y / HH:mm')}}</b> para <b>{{$data->num_person}} personas</b>.</p>
+<p>Su reserva ha sido <b>Confirmada</b> para la fecha <b>{{\Carbon\Carbon::parse($data->fecha_reserva)->isoFormat('dddd D \d\e MMMM \d\e\l Y / HH:mm')}}</b> para <b>{{$data->num_person}} personas</b>.</p>
 <p>La ruta seleccionada es: <b>{{$data->route->name}}, </b> la cual comienza en <b>{{$data->route->inicio_ruta_name}}</b>
     y su precio es de <b>{{$data->cost}} euros!</b></p>
