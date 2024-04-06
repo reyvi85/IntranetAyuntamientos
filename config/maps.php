bc7f2d0983b08fd639631fb54de1d1cd0ceb3c85
Reyvi Bilbao Betancourt <reyvi85@gmail.com>
2024-04-06 16:41:51 -0400

    Agregando variables de entorno de mapas de GOOGLE

--- config/maps.php
+++ config/maps.php
@@ -4,6 +4,6 @@ return[
 
     'API_KEY_GOOGLE_MAP'=>env('API_KEY_GOOGLE_MAP', ''),
 
-    'lat_default'=>40.4165,
-    'lng_default'=>-3.70256,
+    'lat_default'=>env('LAT_DEFAULT', 40.4165,),
+    'lng_default'=>env('LNG_DEFAULT', -3.70256),
 ];
